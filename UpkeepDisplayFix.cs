153374c7238da8df4d5803b7790eff29b7696ea5
WhiteThunder <whetethunger@gmail.com>
2020-12-13 12:28:48 -0800

    Remove unnecessary forDisk check
    Oxide doesn't call OnEntitySaved when saving to disk

--- UpkeepDisplayFix.cs
+++ UpkeepDisplayFix.cs
@@ -1,14 +1,11 @@
 ﻿namespace Oxide.Plugins
 {
-    [Info("Upkeep Display Fix", "WhiteThunder", "0.1.0")]
-    [Description("Fixes the Tool Cupboard UI to properly take into account decay scale.")]
+    [Info("Upkeep Display Fix", "WhiteThunder", "1.0.0")]
+    [Description("Fixes upkeep displayed in the Tool Cupboard UI to properly take into account decay scale.")]
     internal class UpkeepDisplayFix : CovalencePlugin
     {
         private void OnEntitySaved(BuildingPrivlidge buildingPrivilege, BaseNetworkable.SaveInfo saveInfo)
         {
-            if (saveInfo.forDisk)
-                return;
-
             saveInfo.msg.buildingPrivilege.upkeepPeriodMinutes /= ConVar.Decay.scale;
             saveInfo.msg.buildingPrivilege.protectedMinutes /= ConVar.Decay.scale;
         }
